cd51df794913ae697c41d2fee7bccdfd6d74e714
MichaelJauk <jauk.michael14@gmail.com>
2021-02-09 21:16:35 +0100

    Initial commit

---
+++ datasets/Caltech256Dataset.py
@@ -0,0 +1,97 @@
+import torch
+from torch.utils.data.dataset import Dataset
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import matplotlib.pyplot as plt
+from PIL import Image
+import PIL.ImageOps
+import random
+import numpy as np
+
+import torchvision
+from torch.utils.data import DataLoader
+
+
+class Caltech256Dataset(Dataset):
+
+    def __init__(self, root_dir, transform=None, should_invert=False):
+        self.imageFolderDataset = datasets.ImageFolder(root=root_dir)
+        # can be accomplished by self.imageFolderDataset.classes
+        # self.categories = [directory for directory in os.listdir(root_dir)]
+        self.transform = transform
+        self.should_invert = should_invert
+
+    def __getitem__(self, index):
+        # TODO: maybe cut out squares of images before transforming them
+
+        # tuple consists of imagepath and index of category/class
+        img0_tuple = random.choice(self.imageFolderDataset.imgs)
+
+        same_class = random.randint(0, 1)
+        if same_class:
+            while True:
+                img1_tuple = random.choice(self.imageFolderDataset.imgs)
+                # Loop until another image of the same class has been found
+                if img0_tuple[0] != img1_tuple[0] and img0_tuple[1] == img1_tuple[1]:
+                    break
+        else:
+            while True:
+                img1_tuple = random.choice(self.imageFolderDataset.imgs)
+                # Loop until another image of another class has been found
+                if img0_tuple[1] != img1_tuple[1]:
+                    break
+
+        img0 = Image.open(img0_tuple[0]).convert("RGB")
+        img1 = Image.open(img1_tuple[0]).convert("RGB")
+
+        if self.should_invert:
+            img0 = PIL.ImageOps.invert(img0)
+            img1 = PIL.ImageOps.invert(img1)
+
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+
+        return img0, img1, torch.from_numpy(np.array([int(img1_tuple[1] != img0_tuple[1])], dtype=np.float32))
+
+    def __len__(self):
+        return len(self.imageFolderDataset.imgs)
+
+
+
+def imshow(img,text=None,should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic',fontweight='bold',
+            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()    
+
+def main():
+    root_dir = "D:/datasets/256_ObjectCategories/"
+
+    transformations = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+    ])
+
+    dataset = Caltech256Dataset(
+        root_dir=root_dir, transform=transformations, should_invert=False)
+    
+    
+    vis_dataloader = DataLoader(dataset,
+                        shuffle=True,
+                        num_workers=2,
+                        batch_size=8)
+    dataiter = iter(vis_dataloader)
+
+
+    example_batch = next(dataiter)
+    concatenated = torch.cat((example_batch[0],example_batch[1]),0)
+    imshow(torchvision.utils.make_grid(concatenated))
+    print(example_batch[2].numpy())
+
+
+if __name__ == "__main__":
+    main()
---
+++ image-similarity-prototype.py
@@ -0,0 +1,231 @@
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data.dataset import Dataset
+import torchvision.transforms as transforms
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from datasets.Caltech256Dataset import Caltech256Dataset
+
+# Use GPU if possible
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = 'cpu'
+
+
+def show_plot(iteration, loss):
+    plt.plot(iteration, loss)
+    plt.show()
+
+class SiameseNetwork(nn.Module):
+
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+
+        # Load pre-trained VGG-19; structure: https://images.app.goo.gl/MtYeQkBbpEtGfvQE8
+        self.model = torch.hub.load(
+            'pytorch/vision:v0.6.0', 'vgg19', pretrained=True)
+
+        # Remove last two FC layers
+        self.model.classifier = self.model.classifier[:-6]
+        # print(self.model)
+
+    def forward_once(self, x):
+        output = self.model(x)
+        return output
+
+    def forward(self, x1, x2):
+        output1 = self.forward_once(x1)
+        output2 = self.forward_once(x2)
+        return output1, output2
+
+
+class ContrastiveLoss(nn.Module):
+    """
+    Contrastive loss function; using cosine similarity instead of euclidian distance
+
+    TODO: look if this is right, as the cosine similarity calculates the angel between the two vectors; what we need?
+
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
+
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.cosine_similarity = nn.CosineSimilarity()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        """
+        Calculates contrastive loss using cosine similarity though.
+
+        output1: Output 1
+        output2: Output 2
+        label: Similarity label (0 if genuine, 1 if imposter)
+        """
+
+        cos_sim = self.cosine_similarity(output1, output2)
+        # euclidean_distance = F.pairwise_distance(output1, output2)1
+        loss_contrastive = torch.mean((1-label) * torch.pow(cos_sim, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - cos_sim, min=0.0), 2))
+
+        return loss_contrastive
+
+def train(network, criterion, optimizer, epochs, train_dataloader, debug=True):
+    counter = []
+    loss_history = []
+    iteration_number = 0
+
+    for epoch in range(epochs):
+        for i, data in enumerate(train_dataloader, 0):
+            # Load data and move it to gpu if possible
+            img0, img1, label = data
+            img0 = img0.to(device)
+            img1 = img1.to(device)
+            label = label.to(device)
+
+            # Clear out gradients
+            optimizer.zero_grad()
+
+            # Forward both images through network
+            output1, output2 = network(img0, img1)
+
+            # Calcuate contrastive loss and calc gradient
+            loss_contrastive = criterion(output1, output2, label)
+            loss_contrastive.backward()
+
+            # Update parameters
+            optimizer.step()
+
+
+            print(i)
+
+            if debug and i % 10 == 0:
+                print("Epoch number {}\n Current loss {}\n".format(
+                    epoch, loss_contrastive.data))
+                iteration_number += 10
+                counter.append(iteration_number)
+                loss_history.append(loss_contrastive.data)
+
+            del img0, img1, label
+
+    if debug:
+        show_plot(counter, loss_history)
+
+
+def save_model(network, path):
+    torch.save(network.state_dict(), path)
+
+def load_model(path):
+    network = SiameseNetwork()
+    network = network.to(device)
+    network.load_state_dict(torch.load(path))
+    return network
+
+def test_same(network, crit):
+    """
+    To be removed
+    """
+
+    transformations = transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    crit = nn.CosineSimilarity()
+
+    img1 = cv2.imread(
+        r"D:\Dokumente\Schule\Schulstufe_13\SYP-U\PyTorch\image-similarity-prototype\cube_1.jpg")
+    img1 = cv2.resize(img1, (224, 224))
+    img1 = transformations(img1)
+    img1 = img1.unsqueeze(0)
+    img1 = img1.to(device)
+
+    img2 = cv2.imread(
+        r"D:\Dokumente\Schule\Schulstufe_13\SYP-U\PyTorch\image-similarity-prototype\cube_1.jpg")
+    img2 = cv2.resize(img2, (224, 224))
+    img2 = transformations(img2)
+    img2 = img2.unsqueeze(0)
+    img2 = img2.to(device)
+
+    x1, x2 = network(img1, img2)
+    distance = crit(x1, x2)
+    print(distance)
+
+def test_not_same(network, crit):
+    """
+    To be removed
+    """
+    transformations = transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    img1 = cv2.imread(
+        r"D:\Dokumente\Schule\Schulstufe_13\SYP-U\PyTorch\image-similarity-prototype\airpods.jpg")
+    img1 = cv2.resize(img1, (224, 224))
+    img1 = transformations(img1)
+    img1 = img1.unsqueeze(0)
+    img1 = img1.to(device)
+
+    img2 = cv2.imread(
+        r"D:\Dokumente\Schule\Schulstufe_13\SYP-U\PyTorch\image-similarity-prototype\cube_2.jpg")
+    img2 = cv2.resize(img2, (224, 224))
+    img2 = transformations(img2)
+    img2 = img2.unsqueeze(0)
+    img2 = img2.to(device)
+
+    x1, x2 = network(img1, img2)
+    distance = crit(x1, x2)
+    print(distance)
+
+def do_initial_training():
+    # Declare network and move to gpu if possible
+    net = SiameseNetwork()
+    net.to(device)
+
+    # Loss function
+    criterion = ContrastiveLoss()
+
+    # Learning rate
+    lr = 0.0005
+
+    # Optimizer
+    # In this case: Adam
+    # TODO: Look if this is right
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+
+    # epochs to train
+    epochs = 2
+
+    # Initialize dataset
+    transformations = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+    ])
+    
+    # !!! Has to be changed
+    root_dir = "D:/datasets/256_ObjectCategories/"
+    dataset = Caltech256Dataset(
+        root_dir=root_dir, transform=transformations, should_invert=False)
+
+    # Initialize Train dataloader
+    train_dataloader = DataLoader(dataset, shuffle=True, num_workers=0, batch_size=1)
+
+    print("Start training")
+
+    train(net, criterion, optimizer, epochs, train_dataloader)
+
+    PATH = "./model"
+
+    save_model(net, PATH)
+
+    # test_same(net, criterion)
+
+
+def main():
+    do_initial_training()
+
+
+if __name__ == "__main__":
+    main()
